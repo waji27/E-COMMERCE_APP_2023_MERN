2647aa341398db6a3a00325fa084889ba807a0a0
WAJDAN <wajdanakmal37@gmail.com>
2025-05-22 10:20:06 +0500

    User profile update functionality added

--- client/src/pages/UserPages/Profile.jsx
+++ client/src/pages/UserPages/Profile.jsx
@@ -1,24 +1,147 @@
-import Layout from "../../components/Layout";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import UserMenu from "../../components/UserMenu";
+import Layout from "./../../components/Layout";
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Profile = () => {
+  //context
+  const [auth, setAuth] = useAuth();
+  //state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  //get user data
+  useEffect(() => {
+    const { email, name, phone, address } = auth?.user;
+    setName(name);
+    setPhone(phone);
+    setEmail(email);
+    setAddress(address);
+  }, [auth?.user]);
+
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        "http://localhost:3030/api/v1/auth/profile",
+        {
+          name,
+          email,
+          password,
+          phone,
+          address,
+        }
+      );
+      if (data?.errro) {
+        toast.error(data?.error);
+      } else {
+        setAuth({ ...auth, user: data?.updatedUser });
+        let ls = localStorage.getItem("auth");
+        ls = JSON.parse(ls);
+        ls.user = data.updatedUser;
+        localStorage.setItem("auth", JSON.stringify(ls));
+        toast.success("Profile Updated Successfully");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
   return (
     <Layout>
       <section className="bg-white dark:bg-gray-900">
+        {/* <div className="container-fluid m-3 p-3 dashboard">
+          <div className="row">
+            <div className="col-md-3">
+              <UserMenu />
+            </div>
+            <div className="col-md-8">
+              <div className="form-container" style={{ marginTop: "-40px" }}>
+                <form onSubmit={handleSubmit}>
+                  <h4 className="title">USER PROFILE</h4>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      placeholder="Enter Your Name"
+                      autoFocus
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <input
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      placeholder="Enter Your Email "
+                      disabled
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <input
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="form-control"
+                      id="exampleInputPassword1"
+                      placeholder="Enter Your Password"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      placeholder="Enter Your Phone"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      placeholder="Enter Your Address"
+                    />
+                  </div>
+
+                  <button type="submit" className="btn btn-primary">
+                    UPDATE
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div> */}
         <div className="max-w-2xl px-4 py-8 mx-auto lg:py-16">
           <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
-            Update product
+            Update Profile
           </h2>
-          <form action="#">
+          <form onSubmit={handleSubmit}>
             <div className="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
               <div className="sm:col-span-2">
                 <label
                   htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Product Name
+                  Name
                 </label>
                 <input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   type="text"
                   name="name"
                   id="name"
@@ -30,15 +153,17 @@ const Profile = () => {
               </div>
               <div className="w-full">
                 <label
-                  htmlFor="brand"
+                  htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Brand
+                  Email
                 </label>
                 <input
-                  type="text"
-                  name="brand"
-                  id="brand"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="email"
+                  id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   defaultValue="Apple"
                   placeholder="Product brand"
@@ -47,22 +172,24 @@ const Profile = () => {
               </div>
               <div className="w-full">
                 <label
-                  htmlFor="price"
+                  htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Price
+                  Password
                 </label>
                 <input
-                  type="number"
-                  name="price"
-                  id="price"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  name="password"
+                  id="password"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   defaultValue={2999}
                   placeholder="$299"
                   required
                 />
               </div>
-              <div>
+              {/* <div>
                 <label
                   htmlFor="category"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -79,16 +206,18 @@ const Profile = () => {
                   <option value="GA">Gaming/Console</option>
                   <option value="PH">Phones</option>
                 </select>
-              </div>
+              </div> */}
               <div>
                 <label
                   htmlFor="item-weight"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Item Weight (kg)
+                  Address
                 </label>
                 <input
-                  type="number"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  type="text"
                   name="item-weight"
                   id="item-weight"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
@@ -97,7 +226,26 @@ const Profile = () => {
                   required
                 />
               </div>
-              <div className="sm:col-span-2">
+              <div>
+                <label
+                  htmlFor="item-weight"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Phone No.
+                </label>
+                <input
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  type="text"
+                  name="item-weight"
+                  id="item-weight"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  defaultValue={15}
+                  placeholder="Ex. 12"
+                  required
+                />
+              </div>
+              {/* <div className="sm:col-span-2">
                 <label
                   htmlFor="description"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -113,16 +261,16 @@ const Profile = () => {
                     "Standard glass, 3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US"
                   }
                 />
-              </div>
+              </div> */}
             </div>
             <div className="flex items-center space-x-4">
               <button
                 type="submit"
-                className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Update product
+                Update profile
               </button>
-              <button
+              {/* <button
                 type="button"
                 className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
               >
@@ -139,7 +287,7 @@ const Profile = () => {
                   />
                 </svg>
                 Delete
-              </button>
+              </button> */}
             </div>
           </form>
         </div>
